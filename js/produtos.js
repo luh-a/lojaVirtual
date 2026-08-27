dbaa8c2b15b3e7a719a56488f119918a9782a8c1
Anna Luz <anna.mecenas@aluno.senai.br>
2026-08-27 20:39:22 -0300

    criação do conteudo do produtos.js

---
+++ js/produtos.js
@@ -0,0 +1,72 @@
+const produtos = [
+    {
+      id_produto: 1,
+      descricao_produto: "Linha circulo, Harmony",
+      caminho_da_imagem: "../imagens/circulo harmony.jfif",
+      valor_unitario: 45.00,
+      id_secao: 1,
+      nome_secao: "Linhas"
+    },
+    {
+      id_produto: 2,
+      descricao_produto: "Lã cicrulo, Hobby",
+      caminho_da_imagem: "../imagens/circulo hobby.jfif",
+      valor_unitario: 40.00,
+      id_secao: 1,
+      nome_secao: "Lãs"
+    },
+    {
+      id_produto: 3,
+      descricao_produto: "Lã cirulo, Mollet",
+      caminho_da_imagem: "../imagens/circulo mollet.jfif",
+      valor_unitario: 15.00,
+      id_secao: 1,
+      nome_secao: "Lãs"
+    },
+    {
+      id_produto: 4,
+      descricao_produto: "Lã cisne, d'primeira",
+      caminho_da_imagem: "../imagens/cisne d_primeira.jfif",
+      valor_unitario: 10.00,
+      id_secao: 1,
+      nome_secao: "Lãs"
+    },
+  
+    {
+      id_produto: 5,
+      descricao_produto: "Linha euroroma, Fiore",
+      caminho_da_imagem: "../imagens/euroma fiore.jfif",
+      valor_unitario: 30.00,
+      id_secao: 2,
+      nome_secao: "Linhas"
+    },
+    {
+      id_produto: 6,
+      descricao_produto: "Linha euroroma, Milano",
+      caminho_da_imagem: "../imagens/milano novelo.jfif",
+      valor_unitario: 25.00,
+      id_secao: 2,
+      nome_secao: "Linhas"
+    },
+    {
+      id_produto: 7,
+      descricao_produto: "Linha, pack 120",
+      caminho_da_imagem: "../imagens/pack 120.jfif",
+      valor_unitario: 50.00,
+      id_secao: 2,
+      nome_secao: "Linhas"
+    },
+    {
+      id_produto: 8,
+      descricao_produto: "Porta novelo",
+      caminho_da_imagem: "../imagens/porta novelo.jfif",
+      valor_unitario: 45.00,
+      id_secao: 3,
+      nome_secao: "Outros"
+    },
+  ];
+  
+  // Exemplo de como acessar o array no console
+
+
+  export{produtos};
