9f2cb06d9cee99dd415b4385b2eab94ab544601a
luh-a <anna.mecenas@aluno.senai.br>
2026-08-31 20:52:56 -0300

    Update image paths for product entries

--- js/produtos.js
+++ js/produtos.js
@@ -2,7 +2,7 @@ const produtos = [
     {
       id_produto: 1,
       descricao_produto: "Linha circulo, Harmony",
-      caminho_da_imagem: "../imagens/circulo harmony.jfif",
+      caminho_da_imagem: "imagens/circulo harmony.jfif",
       valor_unitario: 45.00,
       id_secao: 1,
       nome_secao: "Linhas"
@@ -10,7 +10,7 @@ const produtos = [
     {
       id_produto: 2,
       descricao_produto: "Lã cicrulo, Hobby",
-      caminho_da_imagem: "../imagens/circulo hobby.jfif",
+      caminho_da_imagem: "imagens/circulo hobby.jfif",
       valor_unitario: 40.00,
       id_secao: 1,
       nome_secao: "Lãs"
@@ -18,7 +18,7 @@ const produtos = [
     {
       id_produto: 3,
       descricao_produto: "Lã cirulo, Mollet",
-      caminho_da_imagem: "../imagens/circulo mollet.jfif",
+      caminho_da_imagem: "imagens/circulo mollet.jfif",
       valor_unitario: 15.00,
       id_secao: 1,
       nome_secao: "Lãs"
@@ -26,7 +26,7 @@ const produtos = [
     {
       id_produto: 4,
       descricao_produto: "Lã cisne, d'primeira",
-      caminho_da_imagem: "../imagens/cisne d_primeira.jfif",
+      caminho_da_imagem: "imagens/cisne d_primeira.jfif",
       valor_unitario: 10.00,
       id_secao: 1,
       nome_secao: "Lãs"
@@ -35,7 +35,7 @@ const produtos = [
     {
       id_produto: 5,
       descricao_produto: "Linha euroroma, Fiore",
-      caminho_da_imagem: "../imagens/euroma fiore.jfif",
+      caminho_da_imagem: "imagens/euroma fiore.jfif",
       valor_unitario: 30.00,
       id_secao: 2,
       nome_secao: "Linhas"
@@ -43,7 +43,7 @@ const produtos = [
     {
       id_produto: 6,
       descricao_produto: "Linha euroroma, Milano",
-      caminho_da_imagem: "../imagens/milano novelo.jfif",
+      caminho_da_imagem: "imagens/milano novelo.jfif",
       valor_unitario: 25.00,
       id_secao: 2,
       nome_secao: "Linhas"
@@ -51,7 +51,7 @@ const produtos = [
     {
       id_produto: 7,
       descricao_produto: "Linha, pack 120",
-      caminho_da_imagem: "../imagens/pack 120.jfif",
+      caminho_da_imagem: "imagens/pack 120.jfif",
       valor_unitario: 50.00,
       id_secao: 2,
       nome_secao: "Linhas"
@@ -59,7 +59,7 @@ const produtos = [
     {
       id_produto: 8,
       descricao_produto: "Porta novelo",
-      caminho_da_imagem: "../imagens/porta novelo.jfif",
+      caminho_da_imagem: "imagens/porta novelo.jfif",
       valor_unitario: 45.00,
       id_secao: 3,
       nome_secao: "Outros"
